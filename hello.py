323c8cff959ff0cc6c3108c6d48043e7c092a0f7
Akanksh J <akankshj@Akankshs-MacBook-Pro.local>
2023-10-24 19:47:38 -0700

    Added code to run with sqlAlchemy

--- hello.py
+++ hello.py
@@ -2,19 +2,34 @@ from flask import Flask
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String,Integer
+from sqlalchemy.orm import Mapped
 
 from sqlalchemy.sql import func
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:Your_Password_Here@localhost:3306/users"
+app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:Your_Password_here@localhost:3306/users"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-@app.route('/')
-def hello():
-    conn = mysql.connect()
-    cursor =conn.cursor()
+db.init_app(app)
 
-    return data
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(20))
+    password: Mapped[str] = mapped_column(String(20))
+
+with app.app_context():
+    db.create_all()
+
+# @app.route('/')
+# def hello():
+#     conn = mysql.connect()
+#     cursor =conn.cursor()
+
+#     return data
