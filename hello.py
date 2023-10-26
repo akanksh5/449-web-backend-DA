e249667b789f92c248efe0d130937e8c0820e8dc
Akanksh J <akankshj@Akankshs-MBP.lan>
2023-10-25 21:12:52 -0700

    Added support for jwt

--- hello.py
+++ hello.py
@@ -6,6 +6,13 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import String,Integer
 from sqlalchemy.orm import Mapped
+from flask import jsonify
+from flask import request
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 from sqlalchemy.sql import func
 class Base(DeclarativeBase):
@@ -14,7 +21,7 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:Your_Password_here@localhost:3306/users"
+app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:Your_Password_Here!@localhost:3306/users"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -24,12 +31,37 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(20))
     password: Mapped[str] = mapped_column(String(20))
 
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = ""  # Change this!
+jwt = JWTManager(app)
+
+
 with app.app_context():
     db.create_all()
 
-# @app.route('/')
-# def hello():
-#     conn = mysql.connect()
-#     cursor =conn.cursor()
+@app.route('/')
+def hello():
+    return "Hello World!"
 
-#     return data
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
