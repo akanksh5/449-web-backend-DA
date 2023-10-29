1602213dec7ac3152e621255b6dd74127582425a
Dhanushsubbaiah <dhanushsubbaiah59@gmail.com>
2023-10-29 11:57:43 -0700

    added code for crud api's

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase,mapped_column, Mapped
 from sqlalchemy import String,Integer
 from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required,JWTManager
+from werkzeug.utils import secure_filename
+import os
 
 from sqlalchemy.sql import func
 class Base(DeclarativeBase):
@@ -12,8 +14,13 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:Your_Password_here!@localhost:3306/users"
+app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:Test!@localhost:3306/movie_system"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+UPLOAD_FOLDER = './files/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 db.init_app(app)
 
 class User(db.Model):
@@ -22,9 +29,14 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(20))
     password: Mapped[str] = mapped_column(String(20))
 
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), nullable=False)
+    moviename: Mapped[str] = mapped_column(String(20),nullable=False)
+    rating:  Mapped[int] = mapped_column(Integer)
 
 # Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = ""  # Change this!
+app.config["JWT_SECRET_KEY"] = "super-secret-1"  # Change this!
 jwt = JWTManager(app)
 
 
@@ -45,7 +57,7 @@ def login():
      #if username not present in db
     #username = None
     if username == None:
-        return jsonify({"msg":"username not found"}), 404
+        return  'bad request!', 400
     if username != "test" or password != "test":
         return jsonify({"msg": "Bad username or password"}), 401
 
@@ -61,3 +73,93 @@ def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/movies')
+def showmovies():
+    # Movie name
+    # Movie Rating
+    # User
+    return "Hello World!"
+
+@app.route('/movie', methods = [ 'POST']) #Create
+@jwt_required()
+def createmovie():
+   #username=request.json.get("username", None)
+    moviename = request.json.get("moviename", None)
+    movierating = request.json.get("movierating", 0)
+    current_user = get_jwt_identity()
+    if moviename == None:
+        return "movie name is absent",404
+    movie = Movie(username=current_user,moviename=moviename,rating=movierating)
+    db.session.add(movie)
+    db.session.commit()
+    # Movie name
+    # Movie Rating
+    # User
+    return "Hello World!"
+
+
+@app.route('/movie/{movieName}', methods = ['GET']) #Read
+def getmovie():
+    # Movie name
+    # Movie Rating
+    # User
+    return "Hello World!"
+
+@app.route('/movie', methods = ['PUT']) #Update
+def updatemovie():
+    # Movie name
+    # Movie Rating
+    # User
+    return "Hello World!"
+
+@app.route('/movie/<moviename>', methods = ['DELETE']) #Delete
+@jwt_required()
+def deletemovie(moviename):
+  #  id = 2
+   # obj = Movie(id=2)
+    print("Hi")
+    current_user = get_jwt_identity()
+    obj =Movie.query.filter_by(username=current_user,moviename=moviename).first()
+    db.session.delete(obj)
+    db.session.commit()
+
+    # Movie name
+    # Movie Rating
+    # User
+    return "Hello World!"
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       f.save(secure_filename(f.filename))
+#       return 'file uploaded successfully'
+     if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        try:
+            os.mkdir(app.config['UPLOAD_FOLDER'])
+        except OSError:
+            print ("Creation of the directory %s failed" % app.config['UPLOAD_FOLDER'])
+        else:
+            print ("Successfully created the directory %s " % app.config['UPLOAD_FOLDER'])
+        if not allowed_file(file.filename):
+            return 'invalid file type uploaded',401
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'file uploaded successfully'
