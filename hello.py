f3d70f1451cc6121a4028a41c87666525a36d709
akanksh5 <akankshjagadish@gmail.com>
2023-10-30 19:41:49 -0700

    final changes in the code

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify,abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase,mapped_column, Mapped
 from sqlalchemy import String,Integer
@@ -6,6 +6,7 @@ from flask_jwt_extended import create_access_token,get_jwt_identity,jwt_required
 from werkzeug.utils import secure_filename
 from os import environ 
 import os
+import werkzeug 
 
 from sqlalchemy.sql import func
 class Base(DeclarativeBase):
@@ -40,13 +41,10 @@ class Movie(db.Model):
 app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY")  # Change this!
 jwt = JWTManager(app)
 
-
+# Create all the tables in the database
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def hello():
-    return "Hello World!"
 
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
@@ -55,18 +53,21 @@ def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     obj =User.query.filter_by(username=username,password=password).first()
+    if username == None or password == None:
+        return jsonify({"msg": "username or password is missing"}), 400
     if obj:
         access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token)
+        return jsonify(access_token=access_token), 200
     return jsonify({"msg": "invalid username or password"}), 401
 
+# function that returns the allowed file extensions
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Function that's used to register the users
 @app.route('/register', methods = [ 'POST']) #Create
 def registeruser():
-   #username=request.json.get("username", None)
     username = request.json.get("username", None)
     password = request.json.get("password", None)
     email = request.json.get("email", None)
@@ -75,16 +76,19 @@ def registeruser():
     user = User(username=username,password=password,email=email)
     db.session.add(user)
     db.session.commit()
-    return "Hello World!"
+    return "The user has been registered",201
 
 
+# The public route that displays the list of movie reviews submitted by all the users
 @app.route('/movies')
 def showmovies():
     movies = Movie.query.filter().all()
     result = []
     for i in movies:
         result.append({'Movie Name':i.moviename,'Rating':i.rating,'Username':i.username})
-    return jsonify({'Movies':result})
+    return jsonify({'Movies':result}), 200
+
+# endpoint used to create a movie rating entry by an authenticated user
 
 @app.route('/movie', methods = [ 'POST']) #Create
 @jwt_required()
@@ -98,9 +102,10 @@ def createmovie():
     movie = Movie(username=current_user,moviename=moviename,rating=movierating)
     db.session.add(movie)
     db.session.commit()
-    return "Hello World!"
+    return "the movie record has been created", 201
 
 
+# endpoint used to read a movie rating entry by an authenticated user
 @app.route('/movie/<movieName>', methods = ['GET']) #Read
 @jwt_required()
 def getmovie(movieName):
@@ -110,6 +115,7 @@ def getmovie(movieName):
     return jsonify({'Movies':result})
 
 
+# endpoint used to update a movie rating entry by an authenticated user
 @app.route('/movie/<moviename>', methods = ['PUT']) #Update
 @jwt_required()
 def updatemovie(moviename):
@@ -119,20 +125,21 @@ def updatemovie(moviename):
         return "movie name is absent",404
     movie = Movie.query.filter_by(username=current_user,moviename=moviename).update(dict(rating=movierating))
     db.session.commit()
-    return "Hello World!"
+    return "the movie record has been updated",201
 
+# endpoint used to delete a movie rating entry by an authenticated user
 @app.route('/movie/<moviename>', methods = ['DELETE']) #Delete
 @jwt_required()
 def deletemovie(moviename):
-    print("Hi")
     current_user = get_jwt_identity()
     obj =Movie.query.filter_by(username=current_user,moviename=moviename).first()
     db.session.delete(obj)
     db.session.commit()
-    return "Hello World!"
+    return "the movie record has been deleted!",204
 
-	
+# endpoint used to upload a file by an authenticated user
 @app.route('/uploader', methods = ['GET', 'POST'])
+@jwt_required()
 def upload_file():
      if request.method == 'POST':
         # check if the post request has the file part
@@ -156,4 +163,24 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return 'file uploaded successfully'
+        return 'file uploaded successfully',204
+     
+# Error handler functions
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    return 'Bad request!', 400
+
+     
+@app.errorhandler(werkzeug.exceptions.Unauthorized)
+def handle_unauthorized_request(e):
+    return 'Unauthorized request!', 401
+
+     
+@app.errorhandler(werkzeug.exceptions.NotFound)
+def handle_bad_request(e):
+    return 'Resource not found!', 404
+
+     
+@app.errorhandler(werkzeug.exceptions.InternalServerError)
+def handle_internal_server_error_request(e):
+    return 'Internal Server Error', 500
